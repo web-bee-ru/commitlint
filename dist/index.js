a0c44b510da5e8628fdd65f716b4b4bbe7a00aa6
santoslos <papoyan.gosha@gmail.com>
2022-02-03 10:41:06 +0300

    feat:  added support for conventional commit

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,20 @@
 "use strict";
-var rule_1 = require("./rule");
+var types_1 = require("@commitlint/types");
 module.exports = function index(params) {
     var configuration = {
         /**
          * Resolve and load @commitlint/config-conventional from node_modules.
          * Referenced packages must be installed
          */
-        // extends: ['@commitlint/config-conventional'],
         /**
          * Any rules defined here will override rules from @commitlint/config-conventional
          */
+        extends: ["@commitlint/config-conventional"],
         // https://commitlint.js.org/#/reference-rules
         rules: {
-            "local-plugin": [2, "always"],
+            "type-enum": [types_1.RuleConfigSeverity.Error, "always", params.commitTypes],
+            "subject-case": [types_1.RuleConfigSeverity.Disabled],
         },
-        /**
-         * Functions that return true if commitlint should ignore the given message.
-         */
-        // ignores: [(commit) => commit === ''],
-        /**
-         * In case you want to develop your plugins locally without the need to publish to npm,
-         * you can send declare your plugins inside your project locally.
-         *
-         * @NOTE: Please be aware that you can declare only one local plugin.
-         */
-        plugins: [
-            {
-                rules: {
-                    "local-plugin": function (_a) {
-                        var raw = _a.raw;
-                        return (0, rule_1.validate)(raw, params);
-                    },
-                },
-            },
-        ],
     };
     return configuration;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 ﻿import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
 
-import { RuleParams, validate } from "./rule";
+type RuleParams = {
+  commitTypes?: string[];
+};
 
 export = function index(params: RuleParams) {
   const configuration: UserConfig = {
@@ -8,37 +11,16 @@ export = function index(params: RuleParams) {
      * Resolve and load @commitlint/config-conventional from node_modules.
      * Referenced packages must be installed
      */
-    // extends: ['@commitlint/config-conventional'],
 
     /**
      * Any rules defined here will override rules from @commitlint/config-conventional
      */
-
+    extends: ["@commitlint/config-conventional"],
     // https://commitlint.js.org/#/reference-rules
     rules: {
-      "local-plugin": [2, "always"],
+      "type-enum": [RuleConfigSeverity.Error, "always", params.commitTypes],
+      "subject-case": [RuleConfigSeverity.Disabled],
     },
-
-    /**
-     * Functions that return true if commitlint should ignore the given message.
-     */
-    // ignores: [(commit) => commit === ''],
-
-    /**
-     * In case you want to develop your plugins locally without the need to publish to npm,
-     * you can send declare your plugins inside your project locally.
-     *
-     * @NOTE: Please be aware that you can declare only one local plugin.
-     */
-    plugins: [
-      {
-        rules: {
-          "local-plugin": ({ raw }) => {
-            return validate(raw, params);
-          },
-        },
-      },
-    ],
   };
   return configuration;
 };
